b6a63b008bf48dc4d37e15c84afd88c862bb087d
MarcinSc <marcins78@gmail.com>
2022-04-30 17:26:37 +0700

    Removed uneccessary uniforms

--- gdx-graph/src/com/gempukku/libgdx/graph/plugin/models/provided/ModelFragmentCoordinateShaderNodeBuilder.java
+++ gdx-graph/src/com/gempukku/libgdx/graph/plugin/models/provided/ModelFragmentCoordinateShaderNodeBuilder.java
@@ -23,12 +23,8 @@ public class ModelFragmentCoordinateShaderNodeBuilder extends ConfigurationCommo
     protected ObjectMap<String, ? extends FieldOutput> buildCommonNode(boolean designTime, String nodeId, JsonValue data, ObjectMap<String, FieldOutput> inputs, ObjectSet<String> producedOutputs, CommonShaderBuilder commonShaderBuilder, GraphShaderContext graphShaderContext, GraphShader graphShader) {
         commonShaderBuilder.addUniformVariable("u_viewportSize", "vec2", true, UniformSetters.viewportSize,
                 "Viewport size");
-        commonShaderBuilder.addUniformVariable("u_pixelSize", "vec2", true, UniformSetters.pixelSize,
-                "Pixel size");
         commonShaderBuilder.addUniformVariable("u_projViewTrans", "mat4", true, UniformSetters.projViewTrans,
                 "Projection-view transformation");
-        commonShaderBuilder.addUniformVariable("u_viewTrans", "mat4", true, UniformSetters.viewTrans,
-                "View transformation");
         commonShaderBuilder.addUniformVariable("u_worldTrans", "mat4", false, ModelsUniformSetters.worldTrans,
                 "Model to world transformation");
 
