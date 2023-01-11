30715362a375b084c422d735339826689766718f
MarcinSc <marcins78@gmail.com>
2023-01-11 19:48:05 +0700

    Using PhysicsSystem from gdx-gemp-lib

--- gdx-graph-test/src/com/gempukku/libgdx/box2d/artemis/sensor/SensorContactListener.java
+++
@@ -1,9 +0,0 @@
-package com.gempukku.libgdx.box2d.artemis.sensor;
-
-import com.badlogic.gdx.physics.box2d.Fixture;
-
-public interface SensorContactListener {
-    void contactBegun(SensorData sensor, Fixture other);
-
-    void contactEnded(SensorData sensor, Fixture other);
-}
