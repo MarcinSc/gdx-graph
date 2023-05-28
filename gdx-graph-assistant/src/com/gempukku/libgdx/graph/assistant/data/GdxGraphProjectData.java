81d1d31a494e94662566a4262f87c8c887f8d234
MarcinSc <marcins78@gmail.com>
2023-05-28 17:35:20 +0700

    Graphs are sorted in menus

--- gdx-graph-assistant/src/com/gempukku/libgdx/graph/assistant/data/GdxGraphProjectData.java
+++ gdx-graph-assistant/src/com/gempukku/libgdx/graph/assistant/data/GdxGraphProjectData.java
@@ -3,10 +3,19 @@ package com.gempukku.libgdx.graph.assistant.data;
 import com.badlogic.gdx.utils.Array;
 import com.gempukku.libgdx.graph.ui.GraphResolver;
 
+import java.util.Comparator;
+
 public class GdxGraphProjectData implements GraphResolver {
+    private static final Comparator<GdxGraphData> graphsComparator = new Comparator<GdxGraphData>() {
+        @Override
+        public int compare(GdxGraphData o1, GdxGraphData o2) {
+            return o1.getName().compareTo(o2.getName());
+        }
+    };
     private final Array<GdxGraphData> graphs = new Array<>();
 
     public Array<GdxGraphData> getGraphs() {
+        graphs.sort(graphsComparator);
         return graphs;
     }
 
